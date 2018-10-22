b378fffdbbb3de70f15851e7394a2ed430ddcae9
Jingyu Liu <jingyuliu@Jingyus-MacBook-Pro.local>
2018-10-21 22:15:21 -0400

    Test some initial values for lab2/main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -4,7 +4,7 @@
 #include "NdegreePolynomialCalculator.h"
 
 int main(int argc, char* argv[]) {	
-	NdegreePolynomialCalculator* cal = new NdegreePolynomialCalculator(3, 20, 1);
+	NdegreePolynomialCalculator* cal = new NdegreePolynomialCalculator(3, 4, 1);
 	
 	std::vector<float> v;
 	cal->calculate(v);
